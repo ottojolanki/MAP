b1406fef143716bf11a7ae7d8b448661f8ac91be
ottojolanki <otaljo@gmail.com>
2017-07-09 20:28:26 -0700

    add parsing

--- getORIs.py
+++ getORIs.py
@@ -8,15 +8,32 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def getlinks(url):
+def get_links(url):
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'lxml')
     links = [link.get('href') for link in soup.find_all('a')]
     return links
+
+def get_ORIS_from_link(link):
+    data = requests.get(link)
+    data_soup = BeautifulSoup(data.text, 'lxml')
+    data_pretags = data_soup.find_all('pre')
+    result  = []
+    for tag in data_pretags:
+        for tag_token in tag:   
+            content = tag_token.string.split('\n')
+            content_tidy = [line.strip() for line in content if not (line.startswith('CITY') or line =='')]
+            result.extend(content_tidy)
+    return result
+        
+        
+    
 base_URL = 'https://www.icpsr.umich.edu/NACJD/ORIs/'
 ORI_root = baseURL + 'STATESoris.html'
-links_from_root = getlinks(ORI_root)
+links_from_root = get_links(ORI_root)
 links_from_root = [base_URL + link for link in links_from_root]
+ORI_dictionary = dict()
+
 
 examplelink = links_from_root[0]
 exdata = requests.get(examplelink)
--- parseMAPdata.py
+++ parseMAPdata.py
@@ -73,56 +73,97 @@ def parseline(line):
     function for parsing MAP data into a python dictionary
     '''
     line_of_data_dictionary = {}
+    #to be clearer i'll take the extra effort to code slices as constants
+    ID = 0
+    STATE = slice(1, 3)
+    ORI = slice(3, 10)
+    GROUP = slice(10, 12)
+    DIVISION = 12
+    YEAR = slice(13, 15)
+    SEQUENCE_NUMBER = slice(15, 20)
+    JUVENILE_AGE = slice(20, 22)
+    CORE_CITY_INDICATION = 22
+    COVERED_BY = slice(23, 30)
+    COVERED_BY_GRP = 30
+    LAST_UPDATE = slice(31, 37)
+    #Field office whose territory covers the agency
+    FIELD_OFFICE = slice(37, 41)
+    #Number of the month that was the last month reported that year by the submitting agency
+    MONTHS_REPORTED = slice(41, 43)
+    #dummy variable for internal use
+    AGENCY_COUNT = 43
+    #NOTE: population 
+    #Population of the city
+    POPULATION = slice(44, 53)
+    #County the city is in 
+    COUNTY = slice(53, 56)
+    #If present, the code of the MSA the city is located in
+    MSA = slice(56, 59)
+    #If city resides in two counties, this is the second largest county population
+    GROUP_1 = slice(59, 74)
+    #If city resides in two counties, this is the population of the third largest county
+    GROUP_2 = slice(74, 89)
+    #NOTE:to get the total population of the city add the three above to get the total population of the city
+    
+    
+    
     #get rid of the end of line character
     line.rstrip('\n') 
-    
+    line_of_data_dictionary['County'] = line[COUNTY]
+    line_of_data_dictionary['Group_2'] = line[GROUP_2]
+    line_of_data_dictionary['Group_1'] = line[GROUP_1]
+    line_of_data_dictionary['MSA'] = line[MSA]
+    line_of_data_dictionary['Population'] = line[POPULATION]
+    line_of_data_dictionary['Agency_count'] = line[AGENCY_COUNT]
+    line_of_data_dictionary['Months_reported'] = line[MONTHS_REPORTED]
+    line_of_data_dictionary['Field_office'] = line[FIELD_OFFICE]
     #position 0 is ID
-    line_of_data_dictionary['ID'] = line[0]
+    line_of_data_dictionary['ID'] = line[ID]
     
     #positions 1-2 contain State
-    line_of_data_dictionary['State'] = line[1:3]
+    line_of_data_dictionary['State'] = line[STATE]
     
     #Positions 3-9 in the string are the ORI Code, Originating Agency identifier
     #to parse further TODO: https://www.icpsr.umich.edu/NACJD/ORIs/2010%20ORIs/ALoris.html
     #contains descriptions of these
-    line_of_data_dictionary['ORI'] = line[3:10]
+    line_of_data_dictionary['ORI'] = line[ORI]
     
     #Positions 10-11 are a geographical GROUP identifier
     #TODO add mapping constant to identifier to parse further
     
-    line_of_data_dictionary['GROUP'] = line[10:12]
+    line_of_data_dictionary['GROUP'] = line[GROUP]
     
     #position 12 is geographical DIVISION
     #TODO add mapping constant
     
-    line_of_data_dictionary['DIVISION'] = line[12]
+    line_of_data_dictionary['DIVISION'] = line[DIVISION]
     
     #Positions 13-14 are a year "85" = 1985, "90" = 1990 etc.
     
-    line_of_data_dictionary['Year'] = line[13:15]
+    line_of_data_dictionary['Year'] = line[YEAR]
     
     #Positions 15-19 are Sequence number that places all the cities in alphabetical order
     #blank for groups 0,8,9
     
-    line_of_data_dictionary['Sequence_number'] = line[15:20]
+    line_of_data_dictionary['Sequence_number'] = line[SEQUENCE_NUMBER]
     
     #Positions 20-21 are juvenile age limit in the state 
     
-    line_of_data_dictionary['Juvenile_age'] = line[20:22]
+    line_of_data_dictionary['Juvenile_age'] = line[JUVENILE_AGE]
     
     #Position 22 is Core City Indication 'Y' is a MSA (metropolitan statistical area) 'N' is not
-    line_of_data_dictionary['Core_city_indication'] = line[22]
+    line_of_data_dictionary['Core_city_indication'] = line[CORE_CITY_INDICATION]
     
     #Positions 23-29 'covered by' ORI of the agency submitting the data.
     #blank if not covered by
-    line_of_data_dictionary['Covered_by'] = line[23:29]
+    line_of_data_dictionary['Covered_by'] = line[COVERED_BY]
     
     #Position 30 is the group of covered by ORI
-    line_of_data_dictionary['Covered_by_grp'] = line[30]
+    line_of_data_dictionary['Covered_by_grp'] = line[COVERED_BY_GRP]
     
     #Positions 31-36 The date the heading or mailing list information
     #was updated (MMDDYY)
-    line_of_data_dictionary['Last_update'] = line[31:37]
+    line_of_data_dictionary['Last_update'] = line[LAST_UPDATE]
     
     
     
